de14503a24230437b55a3db45a8405516e903443
Polthagorn <6610685262@cn330>
2025-11-07 00:44:58 +0700

    login & register frontend

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
 ]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_page(request):
     return render(request, 'accounts/login_page.html')
+
+def register_page(request):
+    return render(request, 'accounts/register_page.html')
