4a335bb2426f96f59cbba30559d1748a461bf1af
Polthagorn <6610685262@cn330>
2025-11-08 19:20:18 +0700

    add login system

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import Profile
 
 # Create your views here.
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")  # redirect to homepage
+        else:
+            messages.error(request, "Wrong username or password")
+            
     return render(request, 'accounts/login_page.html')
 
 def register_page(request):
