9c5e0d7702f01cc665aa4214411ef32dc8a0c9e1
polthagorn <6610685262@cn330>
2025-11-09 19:58:52 +0700

    create user profile page

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('register/', views.register_page, name='register'),
     path('logout/', views.logout_page, name='logout'),
+    path('profile/', views.profile_page, name='profile'),
 ]
